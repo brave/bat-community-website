e3c27ff5eff6fa179b88243366c2da86dfc7a38d
Miyayes <cnguyen@brave.com>
2019-09-28 17:41:58 +0900

    Update podcast.php

--- podcast.php
+++ podcast.php
@@ -1,1 +1,1 @@
-<?php header( 'Location: https://blog.batcommunity.org/2019/09/theweeklybat-sep20-2019/' ) ;  ?>
+<?php header( 'Location: https://blog.batcommunity.org/2019/09/theweeklybat-sep-27-2019/' ) ;  ?>

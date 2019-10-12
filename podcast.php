10333efc404faf345c8afe905ff4a169559ec6fa
Miyayes <cnguyen@brave.com>
2019-10-12 12:17:29 +0900

    Update podcast

--- podcast.php
+++ podcast.php
@@ -1,1 +1,1 @@
-<?php header( 'Location: https://blog.batcommunity.org/2019/10/theweeklybat-october-5-2019/' ) ;  ?>
+<?php header( 'Location: https://blog.batcommunity.org/2019/10/theweeklybat-oct-11-2019/' ) ;  ?>

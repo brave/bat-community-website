1beca5b998e51f8dfaaec21c762a2b5615b653bd
Miyayes <cnguyen@brave.com>
2020-02-16 21:47:28 -0500

    Update podcast

--- podcast.php
+++ podcast.php
@@ -1,1 +1,1 @@
-<?php header( 'Location: https://blog.batcommunity.org/2020/02/the-weekly-bat-feb-7-2020/' ) ;  ?>
+<?php header( 'Location: https://blog.batcommunity.org/2020/02/the-weekly-bat-feb-14-2020/' ) ;  ?>

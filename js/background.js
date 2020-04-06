36c3672e9e2bc2b71cf86bda55c036460cde139a
Victor Guyard <19635051+FlipFloop@users.noreply.github.com>
2020-04-06 19:33:25 -0400

    Update background.js
    Use `const` instead of `var`

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-//var arrayOfBackgrounds = ['images/stars3.jpg', 'images/london-web33.jpg'];
-var arrayOfBackgrounds = ['images/stars4.jpg'];
-    var choice = Math.floor(Math.random() * arrayOfBackgrounds.length);
+//const arrayOfBackgrounds = ['images/stars3.jpg', 'images/london-web33.jpg'];
+const arrayOfBackgrounds = ['images/stars4.jpg'];
+    const choice = Math.floor(Math.random() * arrayOfBackgrounds.length);
     document.querySelector('.background').style.backgroundImage = 'url(' + arrayOfBackgrounds[choice] + ')';

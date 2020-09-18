4523b3ad1938e5bea90328a6495f509371aa375e
Miyayes <cnguyen@brave.com>
2020-09-17 22:04:35 -0400

    Update podcast

--- podcast.php
+++ podcast.php
@@ -1,1 +1,1 @@
-<?php header( 'Location: https://blog.batcommunity.org/2020/09/the-weekly-bat-episode-59/' ) ;  ?>
+<?php header( 'Location: https://blog.batcommunity.org/2020/09/the-weekly-bat-episode-60/' ) ;  ?>
